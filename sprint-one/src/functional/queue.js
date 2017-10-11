8910054ba3331e9a672883f9182a41cabd42a837
Hack Reactor Student <student@hackreactor.com>
2017-10-11 15:00:37 -0700

    Passed all tests for queue.js' functional class

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size() + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var size = someInstance.size();
+    var result;
+    if (size > 0) {
+      result = storage[1];
+      delete storage[1];
+      var keys = Object.keys(storage);
+      for (var i = 0; i < keys.length; i++) {
+        var newKey = keys[i] - 1;
+        storage[newKey] = storage[keys[i]];      
+      }
+      delete storage[size];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
