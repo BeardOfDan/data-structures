492f536267e1898940c31219d85ea1a61a9cfde0
Hack Reactor Student <student@hackreactor.com>
2017-10-11 14:37:46 -0700

    Passed all tests for the stack.js functional class

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,21 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size() + 1] = value;
   };
 
   someInstance.pop = function() {
+    var size = someInstance.size();
+    var result;
+    if (size > 0) {
+      result = storage[size];
+      delete storage[size];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
