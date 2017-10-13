c9421174d062fcb7119168d49d6ccb6b0434c3c3
Hack Reactor Student <student@hackreactor.com>
2017-10-13 14:39:06 -0700

    Passed all tests for binary search tree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,29 +1,35 @@
 var BinarySearchTree = function(value) {
   var obj = Object.create(BinarySearchTree.prototype);
+  
   obj.value = value;
   obj.left = null;
   obj.right = null;
+  
   return obj;
 };
 
 BinarySearchTree.prototype.insert = function(value) {
-  //Base Case
   var current = this.value;
 
+  // Base Case
   if (current === value) {
-    return;
+    return; // don't add a duplicate value
   } else if (value < current) {
+    
     if (this.left === null) {
       this.left = BinarySearchTree(value);
     } else {
       this.left.insert(value);
     }
+
   } else {
+    
     if (this.right === null) {
       this.right = BinarySearchTree(value);
     } else {
       this.right.insert(value);
     }
+
   }
 };
 
@@ -33,22 +39,34 @@ BinarySearchTree.prototype.contains = function(target) {
   if (current === target) {
     return true;
   } else if (target < current) {
+
     if (this.left === null) {
       return false;
     } else {
       return this.left.contains(target);
     }
+
   } else {
+
     if (this.right === null) {
       return false;
     } else {
       return this.right.contains(target);
     }
+
   }
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb) {
-  
+  cb(this.value);
+
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  } 
+
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  }
 };
 
 
