78e2e821e3dff4a7c6e625afa41cfdf68fedbb0d
Hack Reactor Student <student@hackreactor.com>
2017-10-13 10:31:22 -0700

    Passed all tests for set.js.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  var result = false;  
+  if (this._storage[item] !== undefined) {
+    result = true;
+  }
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage[item] !== undefined) {
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+.add - constant
+.contains - constant
+.remove - constant
+
  */
