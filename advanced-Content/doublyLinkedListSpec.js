12470d274d0d5a71ab47c4c185edcd65e13f468c
Hack Reactor Student <student@hackreactor.com>
2017-10-13 16:07:14 -0700

    Made initial tests for the DoublyLinkedLists class

---
+++ advanced-Content/doublyLinkedListSpec.js
@@ -0,0 +1,69 @@
+describe('doublyLinkedList', function() {
+  var doublyLinkedList;
+  
+  beforeEach(function() {
+    doublyLinkedList = DoublyLinkedList();
+  });
+ 
+  it('should have a head and tail', function() {
+    expect(doublyLinkedList).to.have.property('head');
+    expect(doublyLinkedList).to.have.property('tail');
+  });
+
+  it('should have methods named "addToTail", "removeHead", "contains", "addToHead", and "removeTail"', function() {
+    expect(doublyLinkedList.addToTail).to.be.a('function');
+    expect(doublyLinkedList.removeHead).to.be.a('function');
+    expect(doublyLinkedList.contains).to.be.a('function');
+    expect(doublyLinkedList.addToHead).to.be.a('function');
+    expect(doublyLinkedList.removeTail).to.be.a('function');
+  });
+
+  it('should designate a new tail when new nodes are added', function() {
+    doublyLinkedList.addToTail(4);
+    expect(doublyLinkedList.tail.value).to.equal(4);
+    doublyLinkedList.addToTail(5);
+    expect(doublyLinkedList.tail.value).to.equal(5);
+  });
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
