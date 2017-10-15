6d85a993edc074f868927beb3bd44b3253f4da30
Hack Reactor Student <student@hackreactor.com>
2017-10-14 17:06:06 -0700

    Refactor HashTable to use the get and set methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,15 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var count = 0;
+  let bucket = this._storage.get(index) || [];
+  let count = 0;
   
-  let size = Object.keys(this._storage).length;
 
-  for (let key in this._storage) {
-    if (this._storage[key] !== undefined) {
-      count++;
+  for (let i = 0; i < bucket.length; i++) {
+    let tuple = bucket[i];
+    if (tuple[0] === k) {
+      let oldValue = tuple[1];
+      // bucket[i] = [k, v];
+      tuple[1] = v;
+      return oldValue;
     }
+    count++;
   }
+
+  // insert new thing
+
+  bucket.push([k, v]);
+  this._storage.set(index, bucket);
+  
+
+
+
+  // resize code goes here
 
   if (count === (this._limit * .75)) {
     var oldStorage = this._storage;
@@ -23,63 +38,120 @@ HashTable.prototype.insert = function(k, v) {
     this._limit *= 2;
 
     //populate new storage
-    for (let key in oldStorage) { 
-      var bucket = oldStorage[key];
+    //for (let key in oldStorage) {
+    oldStorage.each(function(value, index, collection) { 
+      var bucket = oldStorage.get(index);
   
       for (var i = 0; i < bucket.length; i++) {
         var tuple = bucket[i];
         if (tuple !== undefined) {
-          this.insert(tuple[0], tuple[1]);              
+          this._storage.set(index, tuple);
+
         }
       }
-    }
+    });
   }
+
+
+  // this._storage.each(function(tuple, index) {
+  //   if (tuple !== undefined) {
+  //     if (tuple[0] === k) {
+  //       // tupple[1] = v;
+  //       this._storage.set(index, [k, v]);
+  //       return;
+  //     }
+  //     count++;
+  //   }
+  // });
+
+  // if (count === (this._limit * .75)) {
+  //   var oldStorage = this._storage;
+  //   this._storage = LimitedArray(this._limit * 2);
+  //   this._limit *= 2;
+
+  //   //populate new storage
+  //   //for (let key in oldStorage) {
+  //   oldStorage.each(function(value, index, collection) { 
+  //     var bucket = oldStorage.get(index);
   
-  if (this._storage[index] === undefined) {
-    this._storage[index] = [[k, v]];
-  } else {
+  //     for (var i = 0; i < bucket.length; i++) {
+  //       var tuple = bucket[i];
+  //       if (tuple !== undefined) {
+  //         this._storage.set(index, tuple);
 
-    const bucket = this._storage[index];
+  //       }
+  //     }
+  //   });
+  // }
+  
 
-    for (let i = 0; i < bucket.length; i++) {
-      const tuple = bucket[i];
+  // if (this._storage.get(index) === undefined) {
+  //   this._storage.set(index, [[k, v]]);
+  // } else {
 
-      if (tuple[0] === k) {
-        tuple[1] = v;
-      }
-    }
+  //   const bucket = this._storage.get(index);
 
-    this._storage[index].push([k, v]);
-  }
+  // for (let i = 0; i < bucket.length; i++) {
+  //   const tuple = bucket[i];
+
+  //   if (tuple[0] === k) {
+  //     tuple[1] = v;
+  //   }
+  // }
+
+
+
+  // // this._storage[index].push([k, v]);
+  // this._storage.set(index, [k, v]);
+
+
+      //  ========================================================================================================================
+
+
+  // }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
-  const bucket = this._storage[index];
 
-  if (bucket !== undefined) {
-    for (let i = 0; i < bucket.length; i++) {
-      let tuple = bucket[i];
-      if (tuple[0] === k) {
-        return tuple[1];
-      }
+  var bucket = this._storage.get(index) || [];
+
+  for (let i = 0; i < bucket.length; i++) {
+    let tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
     }
   }
+
+  return undefined;
 };
 
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  const bucket = this._storage[index];
+
+
+  // const bucket = this._storage[index];
+  var bucket = this._storage.get(index);
+
+      
+      //  ========================================================================================================================
+
+
 
   if (bucket !== undefined) {
     for (let i = 0; i < bucket.length; i++) {
       const tuple = bucket[i];
 
       if (tuple[0] === k) {
-        this._storage[index] = bucket.slice(0, i).concat(bucket.slice(i + 1));
+        bucket.splice(i, 1);
+        this._storage.set(index, bucket); 
+
+
+      //  ========================================================================================================================
+
+
       }
     }
   }
@@ -97,27 +169,29 @@ HashTable.prototype.remove = function(k) {
   });
 
   // check if it is too small
-  if ((size <= (this._limit / 4)) && (this._limit >= 8)) {
+  if ((size < (this._limit / 4)) && (this._limit >= 8)) {
     // shrink the hash table
-
-    const oldStorage = this._storage;
-    this._storage = LimitedArray(this._limit / 2);
 
     // update the size of limit
     this._limit /= 2;
 
+    const oldStorage = this._storage;
+    this._storage = LimitedArray(this._limit);
+
+    let self = this;
+
     // iterate through oldStorage and add it to the current hash table
     oldStorage.each(function(value, index, collection) {
       const tuple = value;
-      this._storage.insert(...tuple);
+      if ((tuple !== undefined) && (tuple.length > 0)) {
+        if (tuple[0].length > 0) {
+          self.insert(...tuple);
+        }
+      }
     });
   }
 
 };
-
-
-// To prevent excessive collisions, make your hashTable double in size as soon as 75 percent of the spaces have been filled
-// To save space, make sure the hashTable halves in size when space usage falls below 25 percent
 
 
 
